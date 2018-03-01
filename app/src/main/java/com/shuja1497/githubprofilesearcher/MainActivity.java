75a2cf5207278d7a055f0fca12b0b76a31ee47d3
shuja1497 <shujareshi1497@gmail.com>
2018-03-02 03:41:21 +0530

    using StringRequest for response

--- app/src/main/java/com/shuja1497/githubprofilesearcher/MainActivity.java
+++ app/src/main/java/com/shuja1497/githubprofilesearcher/MainActivity.java
@@ -2,7 +2,10 @@ package com.shuja1497.githubprofilesearcher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,30 +19,60 @@ public class MainActivity extends AppCompatActivity {
     private StringRequest mStringRequest;
     private String mBASE_URL;
     private String mURL;
+    private EditText mEditText;
+    private TextView mTextView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEditText = findViewById(R.id.editText_ueserName);
+        mTextView = findViewById(R.id.textView_repos);
+        mButton = findViewById(R.id.button_search);
+
         // Get a RequestQueue
 //        mQueue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
-        mBASE_URL = "";
-        mURL = "";
+        mBASE_URL = "https://api.github.com/users/";
+    }
+
+
+    public void search(View view) {
+
+        // clear previous searches
+        clearPreviousSearches();
+
+        // search for current repos
+        searchRepos(mEditText.getText().toString());
+    }
+
+    private void searchRepos(String username) {
+        mURL = mBASE_URL+username+"/repos";
 
         mStringRequest = new StringRequest(Request.Method.GET, mURL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
 
+                showRepos(response);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
 
+                mTextView.setText(" error while calling API");
             }
         });
-
         // Add a request
         MySingleton.getInstance(this).addToRequestQueue(mStringRequest);
+
+    }
+
+    private void showRepos(String response) {
+        mTextView.setText(response);
+    }
+
+    private void clearPreviousSearches() {
+        mTextView.setText("");
     }
 }
