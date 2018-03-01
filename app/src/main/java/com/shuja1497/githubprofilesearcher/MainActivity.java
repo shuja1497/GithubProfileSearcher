86b802bccc291a28364b71e8ef67072efd85e00b
shuja1497 <shujareshi1497@gmail.com>
2018-03-02 01:24:57 +0530

    adding a singleton class

--- app/src/main/java/com/shuja1497/githubprofilesearcher/MainActivity.java
+++ app/src/main/java/com/shuja1497/githubprofilesearcher/MainActivity.java
@@ -3,11 +3,25 @@ package com.shuja1497.githubprofilesearcher;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue mQueue;
+    private StringRequest mStringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get a RequestQueue
+        mQueue = MySingleton.getInstance(this.getApplicationContext()).
+                getRequestQueue();
+
+
+        // Add a request
+        MySingleton.getInstance(this).addToRequestQueue(mStringRequest);
     }
 }
---
+++ app/src/main/java/com/shuja1497/githubprofilesearcher/MySingleton.java
@@ -0,0 +1,44 @@
+package com.shuja1497.githubprofilesearcher;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by shuja1497 on 3/2/18.
+ */
+public class MySingleton {
+    private static MySingleton mInstance;
+    private RequestQueue mRequestQueue;
+    private static Context mCtx;
+
+    private MySingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public static synchronized MySingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new MySingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
